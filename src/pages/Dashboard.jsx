8e46538eb6a8625d08ffb3e4c1470dd002d8161e
Sandeep-anthem <sandeep.b@antheminfotech.com>
2025-04-08 12:36:28 +0530

    commited

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { useDashboard } from "../context/DashboardContext";
 const Dashboard = () => {
   const navigate = useNavigate();
   const token = Cookies.get("token");
+
   const { counts, loading } = useDashboard();
 
   useEffect(() => {
@@ -68,7 +69,6 @@ const Dashboard = () => {
             <img
               src="https://res.cloudinary.com/dmyq2ymj9/image/upload/v1743745859/HR-Challenges-2024_4x-1536x927-1-1024x618_pbrsvs.webp"
               alt="Dashboard visualization"
-              loading="lazy"
             />
           </div>
         </div>
@@ -107,7 +107,10 @@ const DashboardCard = React.memo(
             <p className="text-sm font-medium text-gray-500">{title}</p>
             <p className="text-3xl font-bold text-gray-800 mt-1">
               {loading ? (
-                <span className="text-gray-400 animate-pulse">...</span>
+                <svg className="animate-spin h-5 w-5 text-blue-950" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
               ) : (
                 count
               )}
